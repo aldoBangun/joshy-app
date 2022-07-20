6d31001e00d5f57af91322102b6dfb58a404c890
aldoBangun <punk.aldo68@gmail.com>
2022-07-21 03:40:08 +0800

    Modify wrapper element from header to div

--- src/components/candidate/CandidateHeader.js
+++ src/components/candidate/CandidateHeader.js
@@ -2,11 +2,11 @@ import { Container } from "react-bootstrap";
 
 const CandidateHeader = () => {
   return (
-    <header className="bg-primary py-2 mb-5">
+    <div className="bg-primary py-2 mb-5">
       <Container>
         <h3 className="text-white">Top Jobs</h3>
       </Container>
-    </header>
+    </div>
   )
 }
 
