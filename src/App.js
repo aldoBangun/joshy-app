d2bea8add4f97f4f8ad75a50907841aa0864b62c
VerdyNordsten <verdynordste97@gmail.com>
2022-07-23 05:03:53 +0700

    add-hire

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import DisplayNav from "./pages/DisplayNav";
 import HideNav from "./pages/HideNav";
 import Candidate from "./pages/Candidate";
 import Landing from "./pages/landing/index";
+import Login from "./pages/login";
+import RegisterUser from "./pages/register-user";
+import RegisterCompany from "./pages/register-company";
+import Forgot from "./pages/forgot";
+import Hiring from "./pages/hiring";
 
 const App = () => {
   return (
@@ -10,17 +15,20 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route element={<HideNav />}>
-            {/* nanti komentar ini dihapus saja mas Verdy */}
-            {/*  */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register-user" element={<RegisterUser />} />
+            <Route path="/register-company" element={<RegisterCompany />} />
+            <Route path="/forgot" element={<Forgot />} />
           </Route>
           <Route element={<DisplayNav />}>
             <Route path="/" element={<Landing />} />
             <Route path="/candidates" element={<Candidate />} />
+            <Route path="/hiring" element={<Hiring />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
