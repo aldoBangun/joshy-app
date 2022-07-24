66f330f713a4c6b379f8b2ce5931f37f4439c8a4
Mikhael <goodboybutnot@gmail.com>
2022-07-24 21:26:31 +0700

    work on testimonials integration

--- src/components/Landing/molecules/CardCaraousel.js
+++ src/components/Landing/molecules/CardCaraousel.js
@@ -1,28 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 // css
 import styles from "../../../css/landing.module.css";
 
 const CardCaraousel = (props) => {
-  const { src: inputSrc, title: inputTitle, subtitle: inputSubtitle } = props;
+  const { data } = props;
+  const config = {
+      headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImVtYWlsIjoibG9lYmlzLnRlY2hAZ21haWwuY29tIiwicHJvZmlsZVBpY3R1cmUiOiJodHRwczovL3Jlcy5jbG91ZGluYXJ5LmNvbS9ub2N0dXJuY2xvdWQvaW1hZ2UvdXBsb2FkL3YxNjU4NjUyNDg4L3cxbjZtcGZ5cmY2cWdkZ282dXZ0LmpwZyIsImNvdmVySW1hZ2UiOiJodHRwczovL3Jlcy5jbG91ZGluYXJ5LmNvbS9ub2N0dXJuY2xvdWQvaW1hZ2UvdXBsb2FkL3YxNjU4NjUyNDg4L2d3MW0wY2xtNTNweG1sbW5xd21vLmpwZyIsInJvbGVJZCI6MSwiaWF0IjoxNjU4NjY1ODUxLCJleHAiOjE2NTg3NTIyNTF9.E5BZx7PY3sVYt-iKkeyhNnLKlvTWG_EJqXRLSQKmYWc`}
+    }
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+        axios.get(`https://joshy-app.herokuapp.com/profile/${data.userId}`, config)
+        .then((res) => {
+          setUserInfo(res.data)
+        })
+  }, [data])
+
   return (
     <>
       <Card className={`${styles.card_caraousel}`}>
         <Card.Img
           variant="top"
-          src={inputSrc}
+          src={userInfo?.profilePicture}
           className="rounded-circle border border-5 border-warning mb-2 mt-5"
+          crossOrigin="anonymous"
         />
         <Card.Body>
           <Card.Title className="text-center mb-3 fs-3">
-            {inputTitle}
+            {"name" in userInfo && userInfo.name}
+            {"companyName" in userInfo && userInfo.companyName}
           </Card.Title>
           <Card.Subtitle className="mb-3 text-muted text-center">
-            {inputSubtitle}
+            {"job" in userInfo && userInfo.job}
+            {"companyField" in userInfo && userInfo.companyField}
           </Card.Subtitle>
           <Card.Text className="text-center mb-5">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            {data.testimonialMessage}
           </Card.Text>
         </Card.Body>
       </Card>
